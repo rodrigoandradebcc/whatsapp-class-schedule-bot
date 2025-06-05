14144b4b79ff718450f2a20f5463e9cb03dd937c
Rodrigo Andrade <rodrigoandradebcc@gmail.com>
2025-06-05 00:07:00 -0300

    fix: problem count votes

--- index.ts
+++ index.ts
@@ -9,10 +9,10 @@ const TZ = "America/Belem";
 // ── CONSTANTES DA ENQUETE ───────────────────────────────────────────────────────
 
 // GRUPO DE TESTE
-// const GROUP_ID = "120363419276384559@g.us";
+const GROUP_ID = "120363419276384559@g.us";
 
 // GRUPO REAL CT SABOIA
-const GROUP_ID = "559182178645-1552489380@g.us";
+// const GROUP_ID = "559182178645-1552489380@g.us";
 
 const MORNING_OPTIONS = ["6h", "7h", "8h", "9h"];
 const AFTERNOON_AND_EVENING_OPTIONS = [
@@ -30,7 +30,8 @@ const AFTERNOON_AND_EVENING_OPTIONS = [
 ];
 const SATURDAY_OPTIONS = ["7h", "8h", "9h", "10h", "11h", "12h", "13h", "14h"];
 
-const CAPACITY = 16; // máximo de votos por opção
+// const CAPACITY = 16; // máximo de votos por opção
+const CAPACITY = 1; // máximo de votos por opção
 
 // ── TIPAGENS DE ESTADO ─────────────────────────────────────────────────────────
 interface State {
@@ -85,6 +86,8 @@ function buildQuestionForOffset(offsetDays: number): string {
 function countVotesByName(votes: Vote[]): Record<string, number> {
   return votes.reduce((acc, vote) => {
     vote.selectedOptions.forEach((opt) => {
+      console.log("opt", opt);
+
       if (opt.name) acc[opt.name] = (acc[opt.name] || 0) + 1;
     });
     return acc;
@@ -146,6 +149,7 @@ async function checkVotes(
 ): Promise<void> {
   try {
     const { votes } = await client.getVotes(pollId);
+    console.log("DEBUG getVotes:", votes);
     const counts = countVotesByName(votes as Vote[]);
 
     // reabertura de vagas
@@ -278,8 +282,8 @@ async function logAllGroupIds(client: Whatsapp): Promise<void> {
 
   // Agendamento da enquete da manhã: 21:00 de domingo(0) a sexta(5)
   schedule(
-    "0 19 * * 0-4",
-    // "* * * * *",
+    // "0 19 * * 0-4",
+    "* * * * *",
     () => {
       resetMorningPoll().catch(console.error);
     },
