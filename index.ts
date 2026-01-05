24eea74706861bc13d477e460ad20712dc478d8f
Rod Andrade <rodrigoandradebcc@gmail.com>
2026-01-04 23:12:32 -0300

    fix: add 21 pm schedule

--- index.ts
+++ index.ts
@@ -25,7 +25,7 @@ const AFTERNOON_AND_EVENING_OPTIONS = [
   "18h",
   "19h",
   "20h",
-  // "21h",
+  "21h",
   "Off",
 ];
 const SATURDAY_OPTIONS = ["7h", "8h", "9h", "10h", "11h", "12h", "13h", "14h"];
